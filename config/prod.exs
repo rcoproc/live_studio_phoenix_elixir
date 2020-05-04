c09e9a64af2d948c1d2f0b942bcab3bf2ae1e75b
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-05-04 01:56:18 -0300

    Fix check_origin

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,7 @@ config :live_view_studio, LiveViewStudioWeb.Endpoint,
   url: [host: "postgres-free-tier-1.gigalixir.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
+  check_origin: ["//localhost", "//positive-worthwhile-puma.gigalixirapp.com"],
   secret_key_base: "{SECRET_KEY_BASE}"
 
 # Configure your database
