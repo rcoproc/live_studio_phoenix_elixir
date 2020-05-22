50cda8bcbf59251056bf5d1c2a79412d17dc030d
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-05-22 05:01:44 -0300

    Added link clear all

--- lib/live_view_studio_web/live/filter_live.ex
+++ lib/live_view_studio_web/live/filter_live.ex
@@ -4,12 +4,7 @@ defmodule LiveViewStudioWeb.FilterLive do
   alias LiveViewStudio.Boats
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        boats: Boats.list_boats(),
-        type: "",
-        prices: []
-      )
+    socket = assign_defaults(socket)
 
     {:ok, socket, temporary_assigns: [boats: []]}
   end
@@ -29,6 +24,7 @@ defmodule LiveViewStudioWeb.FilterLive do
               <%= price_checkbox(price: price, checked: price in @prices) %>
             <% end %>
           </div>
+            <a href="#" phx-click="clear">Clear All</a>
         </div>
       </form>
 
@@ -62,6 +58,15 @@ defmodule LiveViewStudioWeb.FilterLive do
     socket = assign(socket, params ++ [boats: boats])
     {:noreply, socket}
   end
+
+	def handle_event("clear", _, socket) do
+	  socket = assign_defaults(socket)
+  	{:noreply, socket}
+	end
+
+	defp assign_defaults(socket) do
+	  assign(socket, boats: Boats.list_boats(), type: "", prices: [])
+	end
 
   defp price_checkbox(assigns) do
     assigns = Enum.into(assigns, %{})
